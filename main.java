4ddf1cebe7426257ad85295389c98dbca87063c0
HILARIO NENGARE <38634516+HilarioNengareJr@users.noreply.github.com>
2023-05-23 18:25:34 +0300

    variables and datatypes

---
+++ main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+       //variable declaration
+        int age;
+        boolean isStudent;
+        double cgpa;
+
+        //variable assignment
+        age = 45;
+        isStudent = false;
+        cgpa = 3.5;
+
+        //variables usage
+        int newAge = age + 1;
+        System.out.println(newAge); // prints 46
+
+        //variable initialization
+        int myAge = 13;
+        boolean amStudent = true;
+        double gpa = 4;
+        
+    }
+}
