85e82bc970b3576004805074b790a28e3458f7d9
HILARIO NENGARE <38634516+HilarioNengareJr@users.noreply.github.com>
2023-06-21 22:36:48 +0300

    Create namePane.java
    Created a JOptionaPane.

---
+++ namePane.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+class OptionPaneObject{
+    JFrame frame;
+    String name, message = new String();
+
+    OptionPaneObject(){
+        message = "welcome ";
+        frame = new JFrame();
+        name = JOptionPane.showInputDialog(frame, "Enter your name");
+
+        if (name.isEmpty()) {
+            JOptionPane.showMessageDialog(frame,"Can not be empty", "Something went wrong", JOptionPane.ERROR_MESSAGE);
+        }
+        else {
+            JOptionPane.showMessageDialog(frame, message + " " + name);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       new OptionPaneObject();
+    }
+}
